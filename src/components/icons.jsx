3e20df4f2b6860f89a010d41c3fe450b3867e26e
josevitor555 <koservitor123@gmail.com>
2024-11-22 19:44:01 -0300

    Meu Primeiro componente

---
+++ src/components/icons.jsx
@@ -0,0 +1,9 @@
+import { Heart } from 'lucide-react';
+
+export function Icons() {
+  return (
+    <div className='w-full flex items-center justify-center'>
+      <h1 className='mt-4'> <Heart></Heart> </h1>
+    </div>
+  )
+}
